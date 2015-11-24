833413aca2087a0c7492d8842d97f0b62d571888
Abhiram Reddy <abhi.reddy1@gmail.com>
2015-11-24 16:12:15 -0500

    initial commit

---
+++ js/custom.js
@@ -0,0 +1,36 @@
+/*!
+ * Start Bootstrap - Agency Bootstrap Theme (http://startbootstrap.com)
+ * Code licensed under the Apache License v2.0.
+ * For details, see http://www.apache.org/licenses/LICENSE-2.0.
+ */
+
+// jQuery for page scrolling feature - requires jQuery Easing plugin
+
+
+$(function() {
+    $('a.page-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+});
+
+
+
+$('body').scrollspy({
+    target: '.navbar-fixed-top'
+});
+
+
+$('.navbar-collapse ul li a').click(function() {
+    $('.navbar-toggle:visible').click();
+});
+
+
+  
+ $('.carousel').carousel({
+        interval: 3200 //changes the speed
+    })
+
