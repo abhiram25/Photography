3e3eb0b796f6d5a9e5b3890cb5b4f84dbfb31591
Abhiram Reddy <abhi.reddy1@gmail.com>
2016-03-17 22:15:18 -0400

    changes

--- js/xGallery.js
+++ js/xGallery.js
@@ -42,7 +42,7 @@ var xGallery = function(options,fx){
         id:false,
         obj:false,
         animationType:"fade",
-        animationSpeed: 600,
+        animationSpeed: 900,
         touchControl:true,
         keyControl:true,
         showPageNum:true,           // Page Nummbers
